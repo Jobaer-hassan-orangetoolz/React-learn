34e53191c69d2acd9fe521f3b54038e81baf7ca1
Jobaer-hassan-orangetoolz <jobaer@orangetoolz.com>
2023-07-10 11:54:56 +0600

    vid-6 completed

---
+++ module1.mjs
@@ -0,0 +1,10 @@
+import fifi from './module2.mjs'
+import {f,b,c} from './module2.mjs'
+
+console.log(fifi)
+console.log(f)
+console.log(b)
+console.log(c)
+//if we export normally not default then the name in the both module have to be same..but 
+//for default export its not mandatory.
+
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = 'jobaer';
+const b = 'dip';
+const c = 'mehedi';
+const d = 'orangetoolz';
+
+export default d;
+export {a}
+export {b}
+export {c}
